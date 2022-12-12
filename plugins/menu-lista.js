e4f8c4ce923f45ce49ea0b7e00d9ddd6cb9c19eb
Humanz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-12-12 21:56:02 +0700

    Update menu-lista.js

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -31,35 +31,28 @@ const sections = [
 {
 title: `𝙇𝙄𝙎𝙏𝘼 𝘿𝙀𝙎𝙋𝙇𝙀𝙂𝘼𝘽𝙇𝙀 | 𝘿𝙍𝙊𝙋-𝘿𝙊𝙒𝙉 𝙇𝙄𝙎𝙏`,
 rows: [
-      {title: "💖 𝘾𝙍𝙀𝘼𝘿𝙊𝙍𝘼 : 𝘾𝙍𝙀𝘼𝙏𝙊𝙍 💖", description: null, rowId: `${usedPrefix}creadora`},
-      {title: "💖 𝘿𝙊𝙉𝘼𝙍 : 𝘿𝙊𝙉𝘼𝙏𝙀 💖", description: null, rowId: `${usedPrefix}donar`},
-      {title: "🐈 𝙄𝙉𝙎𝙏𝘼𝙇𝘼𝙍 𝙂𝘼𝙏𝘼 𝘽𝙊𝙏 : 𝙄𝙉𝙎𝙏𝘼𝙇𝙇 𝘾𝘼𝙏 𝘽𝙊𝙏 🐈", description: null, rowId: `${usedPrefix}instalarbot`},
-      {title: "🤖 𝙎𝙀𝙍 𝙎𝙐𝘽𝘽𝙊𝙏 : 𝗕𝗘 𝗦𝗨𝗕 𝗕𝗢𝗧 🤖", description: null, rowId: `${usedPrefix}jadibot`},
-      {title: "🚀 𝙑𝙀𝙇𝙊𝘾𝙄𝘿𝘼𝘿 : 𝙎𝙋𝙀𝙀𝘿 🚀", description: null, rowId: `${usedPrefix}ping`},
-      {title: "🌀 𝙈𝙀𝙉𝙐 𝘾𝙊𝙈𝙋𝙇𝙀𝙏𝙊 : 𝙁𝙐𝙇𝙇 𝙈𝙀𝙉𝙐 🌀", description: null, rowId: `${usedPrefix}allmenu`},
-      {title: "🌟 𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝘾𝙄𝙊𝙉 : 𝙄𝙉𝙁𝙊 𝙈𝙀𝙉𝙐 🌟", description: null, rowId: `${usedPrefix}infomenu`},
-      {title: "🏅 𝙐𝙎𝙐𝘼𝙍𝙄𝙊𝙎 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 : 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 𝙐𝙎𝙀𝙍𝙎 🏅", description: null, rowId: `${usedPrefix}listprem`},
-      {title: "🎟️ 𝙎𝙀𝙍 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 : 𝘽𝙀 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 🎟️", description: null, rowId: `${usedPrefix}pase premium`},
-      {title: "🏆 𝙏𝙊𝙋 𝙈𝙐𝙉𝘿𝙄𝘼𝙇 𝘾𝙇𝘼𝙎𝙄𝙁𝙄𝘾𝘼𝙏𝙊𝙍𝙄𝘼 🏆", description: null, rowId: `${usedPrefix}top`},
-      {title: "🛣️ 𝙈𝙄𝙎𝙄𝙊𝙉𝙀𝙎 : 𝙈𝙄𝙎𝙎𝙄𝙊𝙉𝙎 🛣️", description: null, rowId: `${usedPrefix}inventario 3`},
-      {title: "🏪 𝙏𝙄𝙀𝙉𝘿𝘼 𝙋𝘼𝙍𝘼 𝘾𝙊𝙈𝙋𝙍𝘼𝙍 🏪", description: null, rowId: `${usedPrefix}buy`}, 
-      {title: "🎒 𝙄𝙉𝙑𝙀𝙉𝙏𝘼𝙍𝙄𝙊 : 𝙄𝙉𝙑𝙀𝙉𝙏𝙊𝙍𝙔 🎒", description: null, rowId: `${usedPrefix}buy`}, 
-      {title: "🎡 𝙅𝙐𝙀𝙂𝙊𝙎 𝘿𝙄𝙉𝘼𝙈𝙄𝘾𝙊𝙎 : 𝙂𝘼𝙈𝙀𝙎 🎡", description: null, rowId: `${usedPrefix}juegosmenu`},
-      {title: "🔊 𝙈𝙀𝙉𝙐 𝘿𝙀 𝘼𝙐𝘿𝙄𝙊𝙎 : 𝘼𝙐𝘿𝙄𝙊𝙎 🔊", description: null, rowId: `${usedPrefix}audios`},
-      {title: "🧰 𝙈𝙀𝙉𝙐 𝙈𝙊𝘿𝙄𝙁𝙄𝘾𝘼𝘿𝙊𝙍 𝘿𝙀 𝘼𝙐𝘿𝙄𝙊 🧰", description: null, rowId: `${usedPrefix}audioefectomenu`},
-      {title: "🔰 𝙈𝙀𝙉𝙐 𝘿𝙀 𝙂𝙍𝙐𝙋𝙊 : 𝙂𝙍𝙊𝙐𝙋 🔰", description: null, rowId: `${usedPrefix}grupomenu`},
-      {title: "⚙️ 𝘾𝙀𝙉𝙏𝙍𝙊 𝘿𝙀 𝘾𝙊𝙉𝙁𝙄𝙂𝙐𝙍𝘼𝘾𝙄𝙊𝙉 ⚙️", description: null, rowId: `${usedPrefix}on`}, 
-      {title: "🎈 𝙈𝙀𝙉𝙐 𝙎𝙏𝙄𝘾𝙆𝙀𝙍 𝙔 𝙁𝙄𝙇𝙏𝙍𝙊𝙎 🎈", description: null, rowId: `${usedPrefix}stickermenu`},
-      {title: "🛰️ 𝙈𝙀𝙉𝙐 𝘾𝙊𝙉𝙑𝙀𝙍𝙏𝙄𝘿𝙊𝙍 : 𝘾𝙊𝙉𝙑𝙀𝙍𝙏𝙀𝙍 🛰️", description: null, rowId: `${usedPrefix}convertidormenu`},
-      {title: "✨ 𝙈𝙀𝙉𝙐 𝙀𝙁𝙀𝘾𝙏𝙊𝙎 𝙔 𝙇𝙊𝙂𝙊𝙎 ✨", description: null, rowId: `${usedPrefix}makermenu`}, 
-      {title: "🌅 𝙈𝙀𝙉𝙐 𝙇𝙊𝙂𝙊 2 🌅", description: null, rowId: `${usedPrefix}menulogos2`},
-      {title: "📲 𝙈𝙀𝙉𝙐 𝘿𝙀 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙎 : 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 𝙈𝙀𝙉𝙐 📲", description: null, rowId: `${usedPrefix}descargasmenu`},
-      {title: "🔍 𝙈𝙀𝙉𝙐 𝘿𝙀 𝘽𝙐𝙎𝙌𝙐𝙀𝘿𝘼𝙎 : 𝙎𝙀𝘼𝙍𝘾𝙃 𝙈𝙀𝙉𝙐 🔍", description: null, rowId: `${usedPrefix}buscarmenu`},
-      {title: "⛩️ 𝙍𝘼𝙉𝘿𝙊𝙈 𝙈𝙀𝙈𝙀 : 𝘼𝙉𝙄𝙈𝙀 ⛩️", description: null, rowId: `${usedPrefix}randommenu`},
-      {title: "🔞 𝙈𝙀𝙉𝙐 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 +18 : 𝘾𝙊𝙈𝙈𝘼𝙉𝘿𝙎 +18 🔞", description: null, rowId: `${usedPrefix}hornymenu`},
-      {title: "⚗️ 𝙈𝙀𝙉𝙐 𝙍𝙋𝙂 ⚗️", description: null, rowId: `${usedPrefix}rpgmenu`},
-      {title: "📑 𝙏𝙄𝙋𝙊𝙎 𝘿𝙀 𝙇𝙄𝙎𝙏𝘼𝙎 : 𝙏𝙔𝙋𝙀𝙎 𝙊𝙁 𝙇𝙄𝙎𝙏𝙎 📑", description: null, rowId: `${usedPrefix}listas`},
-      {title: "💎 𝙈𝙀𝙉𝙐 𝙋𝙍𝙊𝙋𝙄𝙀𝙏𝘼𝙍𝙄𝙊(𝘼) : 𝙈𝙀𝙉𝙐 𝙊𝙒𝙉𝙀𝙍 💎", description: null, rowId: `${usedPrefix}ownermenu`},
+      {title: "💖 Owner 💖", description: null, rowId: `${usedPrefix}creadora`},
+      {title: "💖 DONASI 💖", description: null, rowId: `${usedPrefix}donar`},
+      {title: "🐈 Cara install GataBot-MD 🐈", description: null, rowId: `${usedPrefix}instalarbot`},
+      {title: "🤖 JadiBot (maintance} 🤖", description: null, rowId: `${usedPrefix}jadibot`},
+      {title: "🚀 Speed GojoBotzMD 🚀", description: null, rowId: `${usedPrefix}ping`},
+      {title: "🌀 AllMenu 🌀", description: null, rowId: `${usedPrefix}allmenu`},
+      {title: "🌟 InfoMenu 🌟", description: null, rowId: `${usedPrefix}infomenu`},
+      {title: "🏅 ListPremium 🏅", description: null, rowId: `${usedPrefix}listprem`},
+      {title: "🎒 Inventory 🎒", description: null, rowId: `${usedPrefix}buy`}, 
+      {title: "🎡 GameMenu 🎡", description: null, rowId: `${usedPrefix}juegosmenu`},
+      {title: "🧰 AudioMenu 🧰", description: null, rowId: `${usedPrefix}audioefectomenu`},
+      {title: "🔰 GroupMenu 🔰", description: null, rowId: `${usedPrefix}grupomenu`},
+      {title: "⚙️ Pengaturan ⚙️", description: null, rowId: `${usedPrefix}on`}, 
+      {title: "🎈 StickerMenu 🎈", description: null, rowId: `${usedPrefix}stickermenu`},
+      {title: "🛰️ ConvertMenu 🛰️", description: null, rowId: `${usedPrefix}convertidormenu`},
+      {title: "✨ MakerMenu ✨", description: null, rowId: `${usedPrefix}makermenu`},
+      {title: "📲 DownloadMenu 📲", description: null, rowId: `${usedPrefix}descargasmenu`},
+      {title: "🔍 SearchMenu 🔍", description: null, rowId: `${usedPrefix}buscarmenu`},
+      {title: "⛩️ RandomMenu ⛩️", description: null, rowId: `${usedPrefix}randommenu`},
+      {title: "🔞 HorrnyMenu 🔞", description: null, rowId: `${usedPrefix}hornymenu`},
+      {title: "⚗️ RpgMenu ⚗️", description: null, rowId: `${usedPrefix}rpgmenu`},
+      {title: "👨‍💻 OwnerMenu 👨‍💻", description: null, rowId: `${usedPrefix}ownermenu`},
       {title: "📄 𝙏𝙀𝙍𝙈𝙄𝙉𝙊𝙎, 𝘾𝙊𝙉𝘿𝙄𝘾𝙄𝙊𝙉𝙀𝙎 𝙔 𝙋𝙍𝙄𝙑𝘼𝘾𝙄𝘿𝘼𝘿 📄", description: null, rowId: `términos`},
       
 
@@ -70,29 +63,7 @@ let pareja = global.db.data.users[m.sender].pasangan
 let enlace = { contextInfo: { externalAdReply: {title: wm, body: 'support group' , sourceUrl: nna, thumbnail: await(await fetch(img)).buffer() }}}
 //let name = conn.getName(m.sender)
 const listMessage = {
-text: `╭───────────────────❀\n│${ucapan()}\n│💝¸.• *${name}* •.¸💝\n╰───────────────────❀
-╭━━〔 *${wm}* 〕━━⬣
-┃✪ *Tiempo Actual | Current Time*	    
-┃➺ ${time}   
-┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃✪ *Activa durante | Active during* 
-┃➺ ${uptime}
-┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃✪ *Versión de Slamet kopling𓃠*
-┃➺ ${vs}
-┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃✪ *Usuario(s) | Users*
-┃➺ ${Object.keys(global.db.data.users).length} 
-┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃✪ *Modo | Mode*
-┃➺ ${global.opts['self'] ? '𝙋𝙍𝙄𝙑𝘼𝘿𝙊 - 𝙋𝙍𝙄𝙑𝘼𝙏𝙀' : '𝙋𝙐𝘽𝙇𝙄𝘾𝙊 - 𝙋𝙐𝘽𝙇𝙄𝘾'}
-┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃✪ *Chat(s) Prohibido(s) | Chats Denied*
-┃➺ ${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length} 
-┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃✪ *Usuario(s) Prohibido(s) | Denied Users*
-┃➺ ${Object.entries(global.db.data.users).filter(user => user[1].banned).length}
-╰━━━━━━━━━━━━━━━━━━⬣`, footer: `*𝗣𝗮𝗿𝗲𝗷𝗮 ➺ ${pareja ? `${name} 💞 ${conn.getName(pareja)}` : `😺 Sin Pareja`}*`, //${name} ${ucapan()}
+text: `ListMenu GojoBotzMD`, //${name} ${ucapan()}
 title: null,
 buttonText: "𝙇𝙄𝙎𝙏𝘼 𝘿𝙀 𝙈𝙀𝙉𝙐 : 𝙇𝙄𝙎𝙏 𝙈𝙀𝙉𝙐", 
 sections }
@@ -207,69 +178,62 @@ let menu = `╭───────────────────❀\n│
 //menulista
 let menulista = `
 ╭━━━━━━━━━━━━━━━━━━━⬣
-┃ ⚡ 𝙇𝙄𝙎𝙏𝘼 𝘿𝙀 𝙈𝙀𝙉𝙐 ⚡
-┃ 💖 ${name}
+┃ ⚡ ListMenu GojoBotzMD ⚡
 ╰━━━━━━━━━━━━━━━━━━━⬣
 ${readMore}
 ╭━━━━━━━━━━━━━━━━━━━⬣
-┃ 💖 𝘾𝙍𝙀𝘼𝘿𝙊𝙍𝘼 | 𝘾𝙍𝙀𝘼𝙏𝙊𝙍 💖
+┃ 👨‍💻 Owner 👨‍💻
 ┃ _${usedPrefix}creadora_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 💖 𝘿𝙊𝙉𝘼𝙍 | 𝘿𝙊𝙉𝘼𝙏𝙀 💖
+┃ 💖 Donasi 💖
 ┃ _${usedPrefix}donar_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 🚀 𝙑𝙀𝙇𝙊𝘾𝙄𝘿𝘼𝘿 | 𝙎𝙋𝙀𝙀𝘿 🚀
+┃ 🚀 Speed GojoBotzMD 🚀
 ┃ _${usedPrefix}ping_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 🌀 𝙈𝙀𝙉𝙐 𝘾𝙊𝙈𝙋𝙇𝙀𝙏𝙊 | 𝙁𝙐𝙇𝙇 𝙈𝙀𝙉𝙐 🌀
+┃ 🌀 AllMenu 🌀
 ┃ _${usedPrefix}allmenu_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 🏆 𝙇𝙄𝙎𝙏𝘼 𝘿𝙀 𝘾𝙇𝘼𝙎𝙄𝙁𝙄𝘾𝘼𝙏𝙊𝙍𝙄𝘼 🏆
-┃ _${usedPrefix}top_
-┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 🌟 𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝘾𝙄𝙊𝙉 | 𝙄𝙉𝙁𝙊 𝙈𝙀𝙉𝙐 🌟
+┃ 🌟 InfoMenu 🌟
 ┃ _${usedPrefix}infomenu_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 🎡 𝙅𝙐𝙀𝙂𝙊𝙎 𝘿𝙄𝙉𝘼𝙈𝙄𝘾𝙊𝙎 | 𝙂𝘼𝙈𝙀𝙎 🎡
+┃ 🎡 GamesMenu 🎡
 ┃ _${usedPrefix}juegosmenu_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 🔊 𝙈𝙀𝙉𝙐 𝘿𝙀 𝘼𝙐𝘿𝙄𝙊𝙎 | 𝘼𝙐𝘿𝙄𝙊𝙎 🔊
-┃ _${usedPrefix}audios_
-┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 🧰 𝙈𝙀𝙉𝙐 𝙈𝙊𝘿𝙄𝙁𝙄𝘾𝘼𝘿𝙊𝙍 𝘿𝙀 𝘼𝙐𝘿𝙄𝙊 🧰
+┃ 🧰 AudioMenu 🧰
 ┃ _${usedPrefix}audioefectomenu_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 🔰 𝙈𝙀𝙉𝙐 𝘿𝙀 𝙂𝙍𝙐𝙋𝙊 | 𝙂𝙍𝙊𝙐𝙋 🔰
+┃ 🔰 GroupMeny 🔰
 ┃ _${usedPrefix}grupomenu_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ ⚙️ 𝘾𝙀𝙉𝙏𝙍𝙊 𝘿𝙀 𝘾𝙊𝙉𝙁𝙄𝙂𝙐𝙍𝘼𝘾𝙄𝙊𝙉 ⚙️
+┃ ⚙️ Pengaturan Group ⚙️
 ┃ _${usedPrefix}on_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 🎈 𝙈𝙀𝙉𝙐 𝙎𝙏𝙄𝘾𝙆𝙀𝙍 𝙔 𝙁𝙄𝙇𝙏𝙍𝙊𝙎 🎈
+┃ 🎈 StickerMenu 🎈
 ┃ _${usedPrefix}stickermenu_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 🛰️ 𝙈𝙀𝙉𝙐 𝘾𝙊𝙉𝙑𝙀𝙍𝙏𝙄𝘿𝙊𝙍 | 𝘾𝙊𝙉𝙑𝙀𝙍𝙏𝙀𝙍 🛰️
+┃ 🛰️ ConvertMenu 🛰️
 ┃ _${usedPrefix}convertidormenu_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ ✨ 𝙈𝙀𝙉𝙐 𝙀𝙁𝙀𝘾𝙏𝙊𝙎 𝙔 𝙇𝙊𝙂𝙊𝙎 ✨
+┃ ✨ MakerMenu ✨
 ┃ _${usedPrefix}makermenu_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 📲 𝙈𝙀𝙉𝙐 𝘿𝙀 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙎 | 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 📲
+┃ 📲 DownloadMenu 📲
 ┃ _${usedPrefix}descargasmenu_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 🔍 𝙈𝙀𝙉𝙐 𝘿𝙀 𝘽𝙐𝙎𝙌𝙐𝙀𝘿𝘼𝙎 | 𝙎𝙀𝘼𝙍𝘾𝙃 🔍
+┃ 🔍 SearchMenu 🔍
 ┃ _${usedPrefix}buscarmenu_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ ⛩️ 𝙍𝘼𝙉𝘿𝙊𝙈 𝙈𝙀𝙈𝙀 | 𝘼𝙉𝙄𝙈𝙀 ⛩️
+┃ ⛩️ RandomMenu ⛩️
 ┃ _${usedPrefix}randommenu_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 🔞 𝙈𝙀𝙉𝙐 +18 | 𝘾𝙊𝙈𝙈𝘼𝙉𝘿𝙎 +18 🔞
+┃ 🔞 HorrnyMenu 🔞
 ┃ _${usedPrefix}hornymenu_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ ⚗️ 𝙈𝙀𝙉𝙐 𝙍𝙋𝙂 ⚗️
+┃ ⚗️ RpgMenu ⚗️
 ┃ _${usedPrefix}rpgmenu_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 💎 𝙈𝙀𝙉𝙐 𝙋𝙍𝙊𝙋𝙄𝙀𝙏𝘼𝙍𝙄𝙊(𝘼) | 𝙊𝙒𝙉𝙀𝙍 💎
+┃ 💎 OwnerMenu 💎
 ┃ _${usedPrefix}ownermenu_
 ┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 ┃ 📄 𝙏𝙀𝙍𝙈𝙄𝙉𝙊𝙎, 𝘾𝙊𝙉𝘿𝙄𝘾𝙄𝙊𝙉𝙀𝙎 
